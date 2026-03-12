d2f3b2afeab59e9d8ec6636426f098ef649371c4
Abdul Rahman <mohammedabdul.rahman@techwave.com>
2026-03-12 11:42:47 +0530

    first implementation

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+        Front-End Developer with 5.10+ years of experience building scalable, responsive, and user-focused web applications.<br></br>
+        specialize in Angular, React.js and Next.js with strong expertise in JavaScript, TypeScript, and modern UI frameworks.<br/>
+        My focus is on delivering seamless user experiences, optimizing performance, and building reusable components that scale across enterprise applications.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
